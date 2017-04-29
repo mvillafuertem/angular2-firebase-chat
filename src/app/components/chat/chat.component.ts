3bdad4dfa18f3973723c35be8eaa32e99745678b
Miguel <mivimur@hotmail.com>
2017-04-30 00:51:39 +0200

    Se añade la función de recargar la página en el último mensaje

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -10,25 +10,31 @@ import { ChatService } from '../../services/chat/chat.service';
 export class ChatComponent implements OnInit {
 
   message:string = "";
+    div:any
 
-  constructor( public _cs:ChatService) {
-    this._cs.loadMessage()
-            .subscribe( () => {
-              console.log("Messages loaded!");
-            })
-  }
+    constructor( public _cs:ChatService) {
+      this._cs.loadMessage()
+              .subscribe( () => {
+                console.log("Messages loaded!")
 
-  send() {
-    if( this.message.length == 0) {
-      return;
+                setTimeout ( ()=> this.div.scrollTop = this.div.scrollHeight, 50 )
+
+              })
     }
-    this._cs.addMessage ( this.message)
-            .then( () => console.log("Delivered!") )
-            .catch ( (error) => console.error(error))
-    this.message = ""
-  }
 
-  ngOnInit() {
-  }
+    send() {
+      if( this.message.length == 0) {
+        return;
+      }
+      this._cs.addMessage ( this.message)
+              .then( () => console.log("Delivered!") )
+              .catch ( (error) => console.error(error))
+      this.message = ""
+    }
+
+    ngOnInit() {
+      this.div = document.getElementById("app-messages")
+      console.log( this.div )
+    }
 
 }

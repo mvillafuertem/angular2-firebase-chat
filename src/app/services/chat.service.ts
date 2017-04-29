f9baa45eaa006e802db5afa2615a17f3729fa9dd
Miguel <mivimur@hotmail.com>
2017-04-29 14:09:27 +0200

    Desarrollo de la lógica del servicio

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Message } from '../interfaces/message.interface';
 
 @Injectable()
 export class ChatService {
 
   chats: FirebaseListObservable<any[]>;
 
-  constructor(private af: AngularFire) {
-    this.chats = af.database.list('')
+  constructor(private af: AngularFire) { }
+
+  // Carga los mensajes de la base de datos
+  loadMessage(){
+    this.chats = this.af.database.list('/chats', {
+      query: {
+        limitToLast: 20,
+        orderByKey: true,
+      }
+    })
+    return this.chats
+  }
+
+  // Añade los mensajes a la base de datos
+  addMessage( text:string ) {
+    let message:Message = {
+      name: "Miguel",
+      message: text
+    }
+    return this.chats.push( message );
   }
 }
